bd10f5722e34cdc8a39373be4e77c011bbd7fa51
hanyahua <leoabby@outlook.com>
2025-06-20 17:59:25 +0800

    fix: aws_security_group script error

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,31 +43,28 @@ resource "aws_security_group" "emsp_sg" {
   description = "Allow HTTP SSH"
   vpc_id = aws_vpc.main.id
 
-  ingress = [
-    {
+  ingress {
       description = "HTTP"
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
+  }
+  ingress  {
       description = "SSH"
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-  egress = [
-    {
+  }
+
+  egress {
       description = "Allow all outbound traffic"
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
       cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  }
 }
 
 resource "aws_security_group" "rds_sg" {
