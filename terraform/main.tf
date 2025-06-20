97fad761e1d5857ea02645ca58a6204d41ab8af6
hanyahua <leoabby@outlook.com>
2025-06-20 21:02:28 +0800

    mysql use db.t4g.micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,7 +118,7 @@ resource "aws_db_instance" "mysql" {
   skip_final_snapshot = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name = aws_db_subnet_group.main.name
-  instance_class = "db.t2.micro"
+  instance_class = "db.t4g.micro"
   identifier = "mysql"
 }
 
